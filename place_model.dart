5ca4fb02d11e279d1229ee986966e0e4e870d5eb
NTDuong96 <101382200+NTDuong96@users.noreply.github.com>
2024-08-17 14:45:29 +0700

    Create place_model.dart

---
+++ place_model.dart
@@ -0,0 +1,13 @@
+class Place {
+  final String name;
+  final String imageUrl;
+
+  Place({required this.name, required this.imageUrl});
+
+  factory Place.fromJson(Map<String, dynamic> json) {
+    return Place(
+      name: json['name'],
+      imageUrl: json['imageUrl'],
+    );
+  }
+}
